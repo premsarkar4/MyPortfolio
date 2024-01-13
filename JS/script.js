21f05011baf570fbfb82230dc036d9d4eb4c27df
premsarkar4 <sarkarprem900@gmail.com>
2024-01-13 10:11:16 +0530

    first commit

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+// Typing Animation
+
+var typed = new Typed(".typing",
+    {
+        strings: ["Student", "Web Developer", "Web Designer", "Front-End Developer"],
+        typeSpeed: 100,
+        BackSpeed: 60,
+        loop: true
+    })
